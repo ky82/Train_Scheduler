44d36a03954a04a883825506e5cd750870875774
ky82 <barwa@123>
2019-09-11 23:07:14 -0700

    Update javascript to include form validation

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -4,15 +4,11 @@ var trainTime = "";
 var trainFrequency = "";
 var nextArrival = "";
 var minutesAway = "";
-
-// jQuery global variables
 var elTrain = $("#train-name");
 var elTrainDestination = $("#train-destination");
 // form validation for Time using jQuery Mask plugin
 var elTrainTime = $("#train-time").mask("00:00");
 var elTimeFreq = $("#time-freq").mask("00");
-
-
 // Initialize Firebase
 var firebaseConfig = {
     apiKey: "AIzaSyAAVhJbeeTFX8sspjNZRaQWrvKg_3gM9nY",
@@ -25,20 +21,15 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-
-// Assign the reference to the database to a variable named 'database'
 var database = firebase.database();
 
 database.ref("/train-scheduler").on("child_added", function(snapshot) {
-
     //  create local variables to store the data from firebase
     var trainDiff = 0;
     var trainRemainder = 0;
     var minutesTillArrival = "";
     var nextTrainTime = "";
     var frequency = snapshot.val().frequency;
-
-    // compute the difference in time from 'now' and the first train using UNIX timestamp, store in var and convert to minutes
     trainDiff = moment().diff(moment.unix(snapshot.val().time), "minutes");
 
     // get the remainder of time by using 'moderator' with the frequency & time difference, store in var
@@ -46,10 +37,8 @@ database.ref("/train-scheduler").on("child_added", function(snapshot) {
 
     // subtract the remainder from the frequency, store in var
     minutesTillArrival = frequency - trainRemainder;
-
     // add minutesTillArrival to now, to find next train & convert to standard time format
     nextTrainTime = moment().add(minutesTillArrival, "m").format("hh:mm A");
-
     // append to our table of trains, inside tbody, with a new row of the train data
     $("#table-data").append(
         "<tr><td>" + snapshot.val().name + "</td>" +
@@ -60,8 +49,6 @@ database.ref("/train-scheduler").on("child_added", function(snapshot) {
     );
 
     $("span").hide();
-
-    // Hover view of delete button
     $("tr").hover(
         function() {
             $(this).find("span").show();
@@ -70,20 +57,15 @@ database.ref("/train-scheduler").on("child_added", function(snapshot) {
             $(this).find("span").hide();
         });
 
-    // STARTED BONUS TO REMOVE ITEMS ** not finished **
     $("#table-data").on("click", "tr span", function() {
         console.log(this);
         var trainRef = database.ref("/train-scheduler/");
         console.log(trainRef);
     });
 });
-
 // function to call the button event, and store the values in the input form
 var storeInputs = function(event) {
-    // prevent from from reseting
     event.preventDefault();
-
-    // get & store input values
     trainName = elTrain.val().trim();
     trainDestination = elTrainDestination.val().trim();
     trainTime = moment(elTrainTime.val().trim(), "HH:mm").subtract(1, "years").format("X");
@@ -102,33 +84,27 @@ var storeInputs = function(event) {
 
     //  alert that train was added
     alert("Train successuflly added!");
-
     //  empty form once submitted
     elTrain.val("");
     elTrainDestination.val("");
     elTrainTime.val("");
     elTimeFreq.val("");
 };
-
 // Calls storeInputs function if submit button clicked
 $("#btn-add").on("click", function(event) {
     // form validation - if empty - alert
     if (elTrain.val().length === 0 || elTrainDestination.val().length === 0 || elTrainTime.val().length === 0 || elTimeFreq === 0) {
         alert("Please Fill All Required Fields");
     } else {
-        // if form is filled out, run function
         storeInputs(event);
     }
 });
-
 // Calls storeInputs function if enter key is clicked
 $('form').on("keypress", function(event) {
     if (event.which === 13) {
-        // form validation - if empty - alert
         if (elTrain.val().length === 0 || elTrainDestination.val().length === 0 || elTrainTime.val().length === 0 || elTimeFreq === 0) {
             alert("Please Fill All Required Fields");
         } else {
-            // if form is filled out, run function
             storeInputs(event);
         }
     }
